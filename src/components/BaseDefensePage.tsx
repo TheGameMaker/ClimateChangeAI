3a774f615bd9ef05b28dbf0df5de117ca5f0bf9e
TheGameMaker <clarksmiley@comcast.net>
2024-10-25 17:21:07 -0400

    added words to the game page for base defense

--- src/components/BaseDefensePage.tsx
+++ src/components/BaseDefensePage.tsx
@@ -11,6 +11,15 @@ function GameView() {
 
   return (
     <div className="basedefense">
+      <h2>Welcome to AR Base Defense!</h2>
+      <p>
+        This game was originally created for an andorid and was then ported over
+        to the Hololens2 and Quest 3 for true Augmented Reality. The Idea is to
+        bring real time strategy games to the real world so you can play them on
+        your table or floor. Potentially Making games like warhammer more
+        immersive. This is we WebGL form of the app so you can play the game
+        without access to these headsets. I hope you enjoy!
+      </p>
       <Unity
         unityProvider={unityProvider}
         style={{
